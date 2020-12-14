d19df3efdde1209e995972923610a3ca6291c659
Coby <cobyrpilpel2@gmail.com>
2020-12-13 19:17:36 -0500

    commit tree 2f0bce78f128cd616d96e8f60897c05387fd2a56

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
 def unsafe?(speed)
+    if speed > 60 || speed < 40
+        return true
+  
 
+
+    else 
+        return false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+
+   if speed > 60 || speed < 40 ? true :  false;
+return true
+else
+    return false
 end
+end
+
+
 	
 
 
